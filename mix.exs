2879d01fe9d5e2fb4e9c694002cbe082bc07c42a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-27 21:31:33 +0200

    Add package to mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,12 @@ defmodule BadSeed.Mixfile do
     [
       app: :bad_seed,
       version: "0.1.0",
+      description: "Stores and reuses ExUnit seeds from failing test runs.",
+      package: [
+        maintainers: ["Jeff Kreeftmeijer"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/jeffkreeftmeijer/bad_seed"}
+      ],
       elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
